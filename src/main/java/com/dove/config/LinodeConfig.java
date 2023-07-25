26fd83568d9893e7483a6adbe844cb088de57658
Mark <kimzasqwe@naver.com>
2023-07-26 00:05:17 +0900

    spring boot aws starter에서 AWS SDK로 변경

--- src/main/java/com/dove/config/LinodeConfig.java
+++ src/main/java/com/dove/config/LinodeConfig.java
@@ -1,12 +1,10 @@
 package com.dove.config;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentials;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectListing;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +18,20 @@ public class LinodeConfig {
 
     @Bean
     public AmazonS3 amazonS3() {
-        AWSCredentials credentials = new BasicAWSCredentials(
-                linodeProperties.getCredentials().accessKey(),
-                linodeProperties.getCredentials().secretKey()
-        );
-
-        return AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://ap-south-1.linodeobjects.com", linodeProperties.getRegion()))
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        BasicAWSCredentials awsCredentials =
+                new BasicAWSCredentials(linodeProperties.getCredentials().accessKey(), linodeProperties.getCredentials().secretKey());
+        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder
+                                .EndpointConfiguration("https://" + linodeProperties.getRegion() + ".linodeobjects.com", linodeProperties.getRegion())
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(awsCredentials)
+                )
+                .enablePathStyleAccess()
+                .disableChunkedEncoding()
                 .build();
+
+        return amazonS3;
     }
 }
